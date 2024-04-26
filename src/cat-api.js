ceae8edce005468acaf3094ad68648828cadffbb
Ladyjhen <145974043+Ladyjhen@users.noreply.github.com>
2024-04-26 20:49:26 +0800

    commit

--- src/cat-api.js
+++ src/cat-api.js
@@ -2,22 +2,25 @@ const BASE_URL = "https://api.thecatapi.com/v1";
 const API_KEY = "live_3s7JZ4STcvSL9dBQOcjhg30hWd64Bsgf9AZ6igcITdbP1dgq8Oe4wP5LDZmwvg19";
 
 export function fetchBreeds() {
-	return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`).then((res) => {
-		if (!res.ok) {
-			throw new Error(res.status);
-		} else {
-			return res.json();
-		}
+	return fetch(`${BASE_URL}/breeds`, {
+	  headers: {
+		'x-api_key': API_KEY,
+	  },
+	}).then(res => {
+	  if (res.ok) return res.json();
+	  throw new Error(res.status);
 	});
-    
-}
-
-export function fetchCatByBreed(breedId) {
-	return fetch(`${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`).then((res) => {
-		if (!res.ok) {
-			throw new Error(res.status);
-		} else {
-			return res.json();
-		}
+  }
+  export function fetchCatByBreed(breedId) {
+	return fetch(`${BASE_URL}/images/search?breed_ids=${breedId}`, {
+	  headers: {
+		'x-api-key': API_KEY,
+	  },
+	}).then(res => {
+	  if (!res.ok) {
+		throw new Error(res.status);
+	  } else {
+		return res.json();
+	  }
 	});
-}
+  }
