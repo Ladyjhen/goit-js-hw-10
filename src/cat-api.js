56bdb9650dff5b0b4abd45bebb3f90bff3f67be0
Ladyjhen <145974043+Ladyjhen@users.noreply.github.com>
2024-04-26 15:41:47 +0800

    commit

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,5 +1,5 @@
 const BASE_URL = "https://api.thecatapi.com/v1";
-const API_KEY = "live_tKr4IYSQLzyDcnpfmmXMohRhtJFemy4XW1xBvdKb3wBzX1f7kQfxOKmng3gzh4C8";
+const API_KEY = "live_3s7JZ4STcvSL9dBQOcjhg30hWd64Bsgf9AZ6igcITdbP1dgq8Oe4wP5LDZmwvg19";
 
 export function fetchBreeds() {
 	return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`).then((res) => {
